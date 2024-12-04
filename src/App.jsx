cf4bccd1a0cfb6b647774c476dd217cc4c197fae
shelavale3795 <pallavishelavale3795@gmail.com>
2024-12-04 17:29:16 +0530

    store

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import TaskDashboard from "./pages/TaskDashboard/TaskDashboard";
 import { Provider } from "react-redux";
-import { store } from "./redux/Store";
+import store from './redux/store'
 
 
 localStorage.setItem('tasks', JSON.stringify([{"text":"hii","isChecked":false},{"id":1733306837041,"title":"React","description":"Assignment","dueDate":"2024-12-04","completed":true},{"id":1733306886393,"title":"Project ","description":"Own task","dueDate":"2024-12-11","completed":false},{"id":1733306919894,"title":"Interview","description":"On hold","dueDate":"2024-12-03","completed":false},{"id":1733307323964,"title":"create labels for fields","description":"create labels for fields","dueDate":"2024-12-04","completed":false}]));
